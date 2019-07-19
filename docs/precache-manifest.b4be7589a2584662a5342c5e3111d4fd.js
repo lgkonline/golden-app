af75a89ae13b5fb0340cc1a80fa400eb6e422947
Lars Gerrit Kliesing <lgk@patorg.org>
2019-07-19 13:20:03 +0200

    First commit

---
+++ docs/precache-manifest.b4be7589a2584662a5342c5e3111d4fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b4f137128ebec8dfba63d98bf8794f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7547ee701f7ccfe6597c",
+    "url": "/static/css/main.abf4a88d.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "7547ee701f7ccfe6597c",
+    "url": "/static/js/main.029c1571.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
