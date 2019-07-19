318eb501a8e0dbf2e0eb0b8b5a5869bdec5b02ef
Lars Gerrit Kliesing <lgk@patorg.org>
2019-07-19 13:23:20 +0200

    Make that it works in 90% of the cases. 3:30.

---
+++ docs/precache-manifest.068888d5eaadb33912061bd51126a090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344f75d1870ff1ff1e949a7320d5583c",
+    "url": "/golden-app/index.html"
+  },
+  {
+    "revision": "0862d54ba9f0df92b790",
+    "url": "/golden-app/static/css/main.abf4a88d.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/golden-app/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "0862d54ba9f0df92b790",
+    "url": "/golden-app/static/js/main.cacc3da7.chunk.js"
+  },
+  {
+    "revision": "2aa0cedb43bb5caa98ac",
+    "url": "/golden-app/static/js/runtime~main.998c7c46.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4be7589a2584662a5342c5e3111d4fd.js"
+  "/golden-app/precache-manifest.068888d5eaadb33912061bd51126a090.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/golden-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
